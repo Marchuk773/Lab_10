64b3dc0fd500ea72d727e216011c26349724d734
Marchuk Roman <marchukroman228@gmail.com>
2020-04-13 14:59:48 +0300

    added more fields

--- Main.py
+++ Main.py
@@ -2,13 +2,14 @@ from Student import Student
 
 
 def main():
-    first_student = Student("Igor", "Vozny", 88, 167)
-    second_student = Student("Max", "Terzy", 95)
-    third_student = Student("Oleg")
+    first_student = Student("Igor", "Vozny", 88, 167, "IKNI", "PZ", "Default")
+    second_student = Student("Max", "Terzy", 95, 190)
+    third_student = Student("Nastya", "Bezpalko", 90)
     print(first_student)
     Student.set_university("KPI")
     print(second_student)
     print(third_student)
+    students_list = [first_student, second_student, third_student]
 
 
 main()
